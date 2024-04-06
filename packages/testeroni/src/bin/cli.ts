a803a1abd42a6a084025b3e8731070c31f5de6cc
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-06 19:10:31 -0400

    Add CLI tool (#3)

---
+++ packages/testeroni/src/bin/cli.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { Command, } from 'commander';
+import {
+  registerScript as registerScriptForBundle,
+} from './commands/bundle.js';
+import { readFileSync, } from 'fs';
+
+const packageJSONPath = import.meta.url.replace(/\/[^/]+$/, '/../../package.json');
+const { name, version, description, } = JSON.parse(readFileSync(new URL(packageJSONPath), 'utf-8')) as { name: string, version: string, description: string, };
+
+const main = async () => {
+  const program = new Command();
+
+  program
+    .name(name)
+    .description(description)
+    .version(version);
+
+  await Promise.all([
+    registerScriptForBundle,
+  ].map(fn => fn(program)));
+
+  await program.parseAsync(process.argv);
+};
+
+await main();
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export default defineConfig({
   test: {
     include: [
-      'test/bundle.test.ts',
+      'test/tests/**/*.test.ts',
     ],
     exclude: [
       'packages/**/*.test.*',
