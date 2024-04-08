9428ee6d38d06d436fffa33908ffe32cc7e068e2
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2024-04-07 21:50:47 -0400

    Fix type import

--- packages/testeroni/src/test-runner/ClientsideTestRunner/type-guards.ts
+++ packages/testeroni/src/test-runner/ClientsideTestRunner/type-guards.ts
@@ -1,4 +1,4 @@
-import type { SupportedDriver, } from "./types.js";
+import { SupportedDriver, } from "./types.js";
 
 export const isPuppeteer = (driver: SupportedDriver): driver is SupportedDriver.puppeteer => driver === SupportedDriver.puppeteer;
 export const isPlaywright = (driver: SupportedDriver): driver is SupportedDriver.playwright => driver === SupportedDriver.playwright;
